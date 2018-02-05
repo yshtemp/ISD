b61568bc43ffa7d0905cb16feabd25b2f9c7d21c
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-05 22:23:50 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,3 @@
-setup_git() {
-  git config --global user.name "$username"
-  git config --global user.email $email
-}
-
-commit_files() {
-  git add .
-  git commit -m "Sync $TRAVIS_CURRENT_DATE"
-}
-
-upload_files() {
-  git remote add github https://$GIT_OAUTH_TOKEN_CHM@github.com/yshtemp/ISD.git > /dev/null 2>&1
-  git push --quiet --set-upstream github master
-}
-
-
 for i in 0 {01..300}; do curl -s http://www.islamspirit.com/islamspirit_ency_$i.php | grep "chm" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | sort | uniq >> links.txt ; done
 wget -qq -i links.txt
 for file in *.zip; do unzip $file && rm $file ; done
@@ -21,6 +5,3 @@ mkdir files
 mv *.chm files/
 export TRAVIS_CURRENT_DATE=$(date +"%d%m%y-%Hh%Mm")
 cd ${TRAVIS_BUILD_DIR}/IDS/
-setup_git
-commit_files
-upload_files
