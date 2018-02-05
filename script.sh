41f9cf75fd1291fb537f88a9f9cabed5c3a90a06
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-05 20:45:12 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,9 @@ mkdir files
 wget -qq -i links.txt
 for file in *.zip; do unzip $file && rm $file ; done
 mv *.chm files/
+git config --global user.name "$username"
+git config --global user.email $email
+export TRAVIS_CURRENT_DATE=$(date +"%d%m%y-%Hh%Mm")
+cd ${TRAVIS_BUILD_DIR}/IDS/
+git add . && git commit -m "Sync $TRAVIS_CURRENT_DATE"
+git push origin master
