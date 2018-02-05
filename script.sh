7aa78b0b297bb0bf5d30e8096996b62769760cc6
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-05 18:57:32 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 for i in 0 {01..300}; do curl -s http://www.islamspirit.com/islamspirit_ency_$i.php | grep "chm" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | sort | uniq >> links.txt ; done
 mkdir files
-wget -i links.txt
+wget -qq -i links.txt
 for file in *.zip; do unzip $file && rm $file ; done
 mv *.chm files/
