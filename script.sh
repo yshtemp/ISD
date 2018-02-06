9d86cdecededfbf6617e9096b17ca5e47a1ddbb4
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-06 23:06:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,20 @@ for file in *.zip; do unzip $file && rm $file ; done
 mkdir files
 mv *.chm files/
 export TRAVIS_CURRENT_DATE=$(date +"%d%m%y-%Hh%Mm")
-cd ${TRAVIS_BUILD_DIR}/IDS/
+setup_git() {
+  git config --global user.name "$username"
+  git config --global user.email $email
+}
+
+commit_files() {
+  git add .
+  git commit -m "Sync $TRAVIS_CURRENT_DATE"
+}
+
+upload_files() {
+  git remote add github https://$GIT_OAUTH_TOKEN_CHM@github.com/yshtemp/ISD.git > /dev/null 2>&1
+  git push --quiet --set-upstream github master
+}
+setup_git
+commit_files
+upload_files
